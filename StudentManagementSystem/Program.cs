0675c18a888dadaa0d60f02a458b4db2db470d67
Sanjar777 <sanjarrashidov355@gmail.com>
2025-06-18 10:48:38 +0500

    Implemented methods of StudentService and proper validation

--- StudentManagementSystem/Program.cs
+++ StudentManagementSystem/Program.cs
@@ -1,7 +1,9 @@
-﻿using StudentManagementSystem.Services;
+﻿using System.Text.RegularExpressions;
+using StudentManagementSystem.Services;
 using StudentManagementSystem.Utils;
 
 StudentService studentService = new();
+var students = studentService.GetAllStudents();
 
 Console.WriteLine("WELCOME TO STUDENT MANAGEMENT SYSTEM!");
 
@@ -18,29 +20,49 @@ switch (validInput)
 {
     case "1":
     case "students":
-        var students = studentService.GetAllStudents();
-        foreach (var student in students)
+        if (students.Count() > 0)
         {
-            Console.WriteLine($"{student.Id}: {student.FirstName} {student.LastName}");
+            foreach (var student in students)
+            {
+                Console.WriteLine($"{student.Id}: {student.FirstName} {student.LastName}");
+            }
         }
+        else
+            Console.WriteLine("There are not any students");
         break;
 
     case "2":
     case "mark":
+        if (students.Count() > 0)
+        {
+            Console.Write("Enter Id of the student: ");
+            var id = int.Parse(Console.ReadLine() ?? "");
+            Console.Write("Enter subject of the student: ");
+            var subject = Console.ReadLine()?.Trim().Capitalize().RemoveDoubleSpaces() ?? "";
+            Console.Write("Enter grade of the student: ");
+            var grade = int.Parse(Console.ReadLine() ?? "");
+            studentService.MarkStudent(id, subject, grade, students);
+        }
+        else
+            Console.WriteLine("There are not any students to mark");
         break;
 
     case "3":
     case "create":
         Console.Write("Input the first name: ");
-        string name = Console.ReadLine()?.Trim().Capitalize() ?? "";
+        string firstName = Console.ReadLine()?.Trim().Capitalize() ?? "";
         Console.Write("Input the last name: ");
-        string surname = Console.ReadLine()?.Trim().Capitalize() ?? "";
-
-        studentService.CreateStudent(name, surname);
+        string lastName = Console.ReadLine()?.Trim().Capitalize() ?? "";
+        int id2 = students.Count == 0 ? 1 : students.Last().Id + 1;
+        if (Regex.IsMatch(firstName, @"^[a-zA-Z]+$") && Regex.IsMatch(lastName, @"^[a-zA-Z]+$"))
+            students.Add(studentService.CreateStudent(firstName, lastName, id2));
+        else
+            Console.WriteLine("Invalid name");
         break;
 
     case "4":
     case "clear":
+        Console.Clear();
         break;
 
     case "5":
@@ -49,3 +71,5 @@ switch (validInput)
     case "kill":
         break;
 }
+
+studentService.SaveStudentsList(students);
