194bdf01986e14bee7842c97e5354865e4cf1a11
Diyorbek <xojimurodovdiyorbek@gmail.com>
2025-06-25 10:42:51 +0500

    all controllers

---
+++ StudentManagementSystem/Models/MarkDto.cs
@@ -0,0 +1,7 @@
+namespace StudentManagementSystem.Models;
+public class MarkDto
+{
+    public int Id { get; set; }
+    public string Subject { get; set; } = string.Empty;
+    public double Value { get; set; }
+}
